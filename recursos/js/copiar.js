736c6ace86a7d0b2d0729105c39dca9361209dd3
Lidia <97204663+Lilly0806@users.noreply.github.com>
2025-12-02 08:55:56 -0300

    Add files via upload

---
+++ recursos/js/copiar.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const botoes = document.querySelectorAll(".btn-copiar");
+
+  botoes.forEach(botao => {
+    botao.addEventListener("click", () => {
+      const caixa = botao.closest(".code-box");
+      const codigo = caixa.querySelector("code").innerText;
+
+      navigator.clipboard.writeText(codigo);
+
+      // ALERTA SIMPLES
+      alert("Código copiado!");
+    });
+  });
+});
+
+
+function mostrarToast() {
+  const toast = document.getElementById("toast-copy");
+  toast.classList.add("show");
+
+  setTimeout(() => {
+    toast.classList.remove("show");
+  }, 1500);
+}
+
+
