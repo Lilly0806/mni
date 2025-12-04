09c0a0fd1b5be5be40ee82a2d04eacfcfc0b376d
Lidia <97204663+Lilly0806@users.noreply.github.com>
2025-12-04 12:54:01 -0300

    Add files via upload

---
+++ recursos/js/copiar.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const botoes = document.querySelectorAll(".btn-copiar");
+
+  botoes.forEach(botao => {
+    botao.addEventListener("click", () => {
+      const caixa = botao.closest(".code-box", ".code-mini", ".code-container");
+      const codigo = caixa.querySelector("code").innerText;
+
+      navigator.clipboard.writeText(codigo);
+
+      // ALERTA SIMPLES
+      alert("Código copiado!");
+    });
+  });
+});
+
+
+
+
+
+
+document.querySelectorAll(".btn-copiar").forEach(btn => {
+    btn.addEventListener("click", () => {
+
+        const container = btn.closest(".code-container");
+        const codigo = container.querySelector("code").innerText.trim();
+
+        navigator.clipboard.writeText(codigo);
+        mostrarToast(); // aparece a mensagem "copiado"
+    });
+});
+function mostrarToast() {
+    const toast = document.getElementById("toast");
+    toast.classList.add("show");
+
+    setTimeout(() => {
+        toast.classList.remove("show");
+    }, 2000);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const botoes = document.querySelectorAll(".btn-copiar");
+
+  botoes.forEach(botao => {
+    botao.addEventListener("click", () => {
+      const caixa = botao.closest(".code-container");
+      const codigo = caixa.querySelector("code").innerText;
+
+      navigator.clipboard.writeText(codigo);
+
+      // ALERTA SIMPLES
+      alert("Código copiado!");
+    });
+  });
+});
